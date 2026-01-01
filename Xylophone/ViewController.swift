d7787aaa9973aec514d2224dace3bc748200a81e
kleeCollage <klee.collage@gmail.com>
2026-01-01 12:36:13 -0600

    feat: Implement logic to make each key play a sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.titleLabel?.text ?? "No title")
+        let key: String = sender.titleLabel?.text ?? "No title"
+        playSound(soundName: key)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 
     
 
